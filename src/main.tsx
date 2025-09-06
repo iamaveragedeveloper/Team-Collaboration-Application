fe0742643783fe8526b1bfcb4e7b153e6b182552
Nihal <iamaveragedeveloper@gmail.com>
2025-09-06 12:34:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+// src/main.tsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // We will create this next
+// FIX: Remove the .tsx extension from the import path
+import App from './App' 
 import './index.css' // Your main CSS file
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
